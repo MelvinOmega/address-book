e08949142b44dd03d01a4cba7dab02cc74ceabc8
Melvin Omega <melvinomega151@gmail.com>
2021-07-28 19:14:13 +0300

    update html, css and js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,16 @@
 function Contact(first, last) {
     this.firstName = first;
     this.lastName = last;
+    this.addresses = [];
   }
+function Address(street, city, county) {
+  this.street = street;
+  this.city = city;
+  this.county = county;
+}
+ Address.prototype.fullAddress = function() {
+   return this.street + " ," + this.city + "," + this.county;
+ }
   Contact.prototype.fullName = function() {
     return this.firstName + " " + this.lastName;
   }
@@ -16,13 +25,29 @@ function Contact(first, last) {
       var inputtedLastName = $("input#new-last-name").val();
   
       var newContact = new Contact(inputtedFirstName, inputtedLastName);
+      $(".new-address").each(function(){
+        var inputtedStreet = $(this).find("#new-street").val();
+        var inputtedCity = $(this).find("#new-city").val();
+        var inputtedCounty = $(this).find("#new-county").val();
+
+        var newAddress = new Address(inputtedStreet, inputtedCity, inputtedCounty);
+
+        newContact.addresses.push(newAddress);
+
+      })
   
       $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
       $(".contact").last().click(function() {
         $("#show-contact").show();
-        $("#show-contact h2").text(newContact.firstName);
-        $(".first-name").text(newContact.firstName);
-        $(".last-name").text(newContact.lastName);
+        $("#show-contact h2").text(newContact.fullName());
+        $("#first-name").text(newContact.firstName);
+        $("#last-name").text(newContact.lastName);
+
+        $("#addresses").text("");
+        newContact.addresses.forEach(function(address){
+          $("#addresses").append("<li>" + address.fullAddress() + "</li>")
+        })
+
       }); 
         
       $("input#new-first-name").val("");
